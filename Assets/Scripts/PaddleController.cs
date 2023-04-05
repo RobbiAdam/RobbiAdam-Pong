3d871660d65007b9f4f1fcf4db40d2540fde50ec
RobbiAdam <100307218+RobbiAdam@users.noreply.github.com>
2023-04-05 21:51:24 +0700

    Pong 2D
    Base Gameplay

---
+++ Assets/Scripts/PaddleController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour
+{
+
+    public float speed;
+    public KeyCode UpKey;
+    public KeyCode DownKey;
+    private Rigidbody2D rig;
+
+    private void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        MoveObject(GetInput());
+    }
+
+    private Vector2 GetInput()
+    {       
+        //get input
+        if(Input.GetKey(UpKey))
+        {
+            return Vector2.up * speed;
+        }
+        else if(Input.GetKey(DownKey))
+        {
+            return Vector2.down * speed;
+        }
+        return Vector2.zero;
+    }
+
+    private void MoveObject(Vector2 movement)
+    {
+        rig.velocity = movement;
+    }
+}
